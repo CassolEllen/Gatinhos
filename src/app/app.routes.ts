729ff1807fecc1832ad492b5d3859e963959f310
Ellen <ellencassol89@gmail.com>
2025-09-02 20:38:18 -0300

    Adicionando loja-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProdutoComponent } from './component/produto-component/produto-component';
 import { HomeComponent } from './component/home-component/home-component';
+import { LojaComponent } from './loja-component/loja-component';
 
 export const routes: Routes = [
 {path: 'produtos', component: ProdutoComponent},
-{path: '', component: HomeComponent}
+{path: '', component: HomeComponent},
+{path: 'lojas', component: LojaComponent}
 
 ];
---
+++ src/app/loja-component/loja-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loja-component',
+  imports: [],
+  templateUrl: './loja-component.html',
+  styleUrl: './loja-component.css'
+})
+export class LojaComponent {
+
+}
